a135b7e2bc34c87c8bc388835d04a94d114d3cc1
Simion Ababi <falltronik@mail.ru>
2020-03-08 17:49:01 -0700

    component update Lifecycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const persons = props =>
-  props.persons.map((person, index) => {
-    return (
-      <div>
-        <Person
-          click={() => props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          changed={event => props.changed(event, person.id)}
-          key={person.id}
-        />
-      </div>
-    );
-  });
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDriveStateFromProps", props);
+  //   return state;
+  // }
 
-export default persons;
+  // componentWillReceiveProps(props) {
+  //   console.log("[Persons.js] componentWillReceiveProps", props);
+  // }
+
+  // componentWillUpdate() {
+  //   console.log("[Persons.js] componentWillUpdate");
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+  getSnapshotBeforeUpdate() {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevStates, snapshot) {
+    console.log("[Persons.js] componentDidUpdate", snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <div>
+          <Person
+            click={() => this.props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            changed={event => this.props.changed(event, person.id)}
+            key={person.id}
+          />
+        </div>
+      );
+    });
+  }
+}
+
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] cosntructor");
+  }
+
   state = {
     persons: [
       { id: "ref", name: "Simon", age: 26 },
@@ -15,6 +20,19 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDriveStateFromProps", props);
+    return state;
+  }
+
+  // componentDidMount() {
+  //   console.log("[App.js] componentDidMount");
+  // }
+
+  componentWillMount() {
+    console.log("[App.js] componentWillMount");
+  }
 
   deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
@@ -50,6 +68,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
 
     if (this.state.showPersons) {
@@ -65,6 +84,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
